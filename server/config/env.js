f9d14d67d19773031f9367895e33fae2c5d680b1
tahanaveed107 <tahanaveed.107@gmail.com>
2025-02-04 12:30:23 +0500

    Setting jwt token for user login

---
+++ server/config/env.js
@@ -0,0 +1,1 @@
+MONGODB_URI = `mongodb+srv://TahaNaveed:tahanaveed.107@book-store-mern.ycfgg.mongodb.net/`;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,22 @@
-import express from 'express';
-import connectDB from './config/connection.js';
-import userRoutes from "./routes/userRoutes.js"
-import dotenv from "dotenv"
+import express from "express";
+import connectDB from "./config/connection.js";
+import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send("Hello World from Express JS");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World from Express JS");
+});
 
 app.use("/users", userRoutes);
 
-
-
-
+const PORT = 4000;
 connectDB();
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server running at port ${process.env.PORT}`);
-    // console.log(`${process.env.MONGODB_URI}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+  // console.log(`${process.env.MONGODB_URI}`);
+});
