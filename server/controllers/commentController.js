d42753338a6af063a644001805f2af50cb911ec0
tahanaveed107 <tahanaveed.107@gmail.com>
2025-02-14 21:57:45 +0500

    added addComment controller

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -3,5 +3,40 @@ import Post from "../models/post.model.js";
 
 export const addComment = async (req, res) => {
   try {
-  } catch (error) {}
+    const userID = req.user.id;
+    const { postID, text } = req.body;
+
+    if (!postID || !text) {
+      return res.status(400).json({
+        message: "Post ID and Comment Text is required",
+      });
+    }
+
+    const post = await Post.findById(postID);
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not Found",
+      });
+    }
+    const newComment = new Comment({
+      user: userID,
+      post: postID,
+      text,
+    });
+
+    const savedComment = await newComment.save();
+
+    post.comments.push(savedComment._id);
+    await post.save();
+
+    return res.status(201).json({
+      message: "Comment added successfully",
+      comment: savedComment,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server Error",
+      error: error.message,
+    });
+  }
 };
