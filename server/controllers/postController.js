b34e78506524038f25ba0108362784b40515a4ea
tahanaveed107 <tahanaveed.107@gmail.com>
2025-03-03 17:28:40 +0500

    created delete post controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -125,14 +125,35 @@ export const editPost = async (req, res) => {
     await post.save();
 
     return res.status(200).json({
-      message: "User Updated Successfully",
-      post: {
-        caption,
-      },
+      message: "Post Updated Successfully",
+      post,
     });
   } catch (error) {
     return res.status(500).json({
       message: " Server Error ",
+      error: error.message,
+    });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const userID = req.user.id;
+    if (!userID) {
+      return res.status(404).json({
+        message: "Authentication failed, Login Again",
+      });
+    }
+    const postID = req.params.postID;
+
+    await Post.findByIdAndDelete(postID);
+
+    return res.status(200).json({
+      message: "Post Deleted Succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server Error",
       error: error.message,
     });
   }
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createPost,
+  deletePost,
   editPost,
   getPosts,
 } from "../controllers/postController.js";
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/", authenticateToken, getPosts);
 router.post("/create", authenticateToken, createPost);
 router.patch("/:postID/edit", authenticateToken, editPost);
+router.delete("/:postID/delete", authenticateToken, deletePost);
 
 export default router;
