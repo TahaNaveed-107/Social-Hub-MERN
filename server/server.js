74248b1eab063bd6501dc8b256369678b9d8c400
tahanaveed107 <tahanaveed.107@gmail.com>
2025-03-07 01:15:19 +0500

    chnaged from local to network acces for backend

--- server/server.js
+++ server/server.js
@@ -22,6 +22,6 @@ app.use("/post", postRoutes);
 
 const PORT = process.env.PORT;
 connectDB();
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
